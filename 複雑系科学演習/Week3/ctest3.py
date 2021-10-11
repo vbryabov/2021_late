61abbbd770e396ac5dd9eb0e742e448fe8de6149
Nishin-0141 <shinnosukepotato@gmail.com>
2021-10-11 16:47:50 +0900

    微調整

--- 複雑系科学演習/Week3/ctest3.py
+++ 複雑系科学演習/Week3/ctest3.py
@@ -9,31 +9,30 @@ class Ctest3():
         self.ax1 = fig.add_subplot(1, 2, 1)
         self.ax2 = fig.add_subplot(1, 2, 2)
 
-
     def code_problem1(self) -> None:
-        plt_array = []
+        ax1_array = []
         for i in self.x:
             num = i
             for j in range(200):
                 num = self.r * num * (1 - num)
-            plt_array.append(num)
+            ax1_array.append(num)
         self.ax1.set_xlabel('$x_0$')
         self.ax1.set_ylabel('$x_{200}$')
-        self.ax1.plot(self.x, plt_array, marker='.', linestyle='None')
+        self.ax1.plot(self.x, ax1_array, marker='.', linestyle='None')
 
     def code_problem2(self) -> None:
-        plt_arrray = []
+        ax2_array = []
         x_array = []
         for i in self.x:
             num = i
             for j in range(200):
                 num = self.r * num * (1 - num)
                 if 150 <= j:
-                    plt_arrray.append(num)
+                    ax2_array.append(num)
                     x_array.append(i)
         self.ax2.set_xlabel('$x_0$')
         self.ax2.set_ylabel('$x_{i}:(150 < i < 200)$')
-        self.ax2.plot(x_array, plt_arrray, marker='.', linestyle='None')
+        self.ax2.plot(x_array, ax2_array, marker='.', linestyle='None')
 
     def show_graph(self) -> None:
         fname = 'images/' + self.s
