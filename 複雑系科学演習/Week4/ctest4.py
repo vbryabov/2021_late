dba16aedb6f873bab365b6cf2efb40df5e539d55
Nishin-0141 <shinnosukepotato@gmail.com>
2021-10-28 21:20:06 +0900

    第３回までの記入

--- 複雑系科学演習/Week4/ctest4.py
+++ 複雑系科学演習/Week4/ctest4.py
@@ -1,6 +1,8 @@
 from matplotlib import pyplot as plt
 import matplotlib
 import numpy as np
+import random
+
 # 日本語フォント用（Linux）
 matplotlib.rc('font', family='Noto Sans CJK JP')
 '''
@@ -16,7 +18,8 @@ class Ctest4():
         self.ax1 = fig.add_subplot(2, 1, 1)
         self.ax2 = fig.add_subplot(2, 1, 2)
 
-        self.x = 0.1    # 初期値を0.1と仮定
+        # self.x = 0.1    # 初期値を0.1と仮定
+        self.x = random.uniform(0, 1)
         # 固定点
         self.u1 = list(map(lambda x: 0, self.r))
         self.u2 = list(map(lambda x: 1 - 1 / x, self.r))
@@ -28,6 +31,8 @@ class Ctest4():
         self.lambda2 = list(map(lambda x: 2 - x, self.r))
         self.lambda3 = self.r2
         self.lambda4 = list(map(lambda x: 2 - x, self.r2))
+        
+        self.filepath = "複雑系科学演習/Week4/images/"
 
     def proprocessing(self, r: float) -> list:
         '''グラフに入れるための前処理'''
@@ -43,7 +48,7 @@ class Ctest4():
         return x_array, r_array
 
     def code_problem1(self):
-        self.ax1.set_title('初期値$x_0 = 0.1$のときのロジスティック写像の分岐図')
+        self.ax1.set_title('初期値$x_0 = {}$（ランダム）のときのロジスティック写像の分岐図'.format(round(self.x, 3)))
         self.ax1.set_xlabel('$r$')
         self.ax1.set_ylabel('$x_n$')
         for i in range(len(self.r)):
@@ -56,7 +61,7 @@ class Ctest4():
                 self.ax1.scatter(result[1], result[0],  color='b', s=1)
 
     def code_problem2(self):
-        self.ax2.set_title('初期値$x_0 = 0.1$のときのロジスティック写像の分岐図')
+        self.ax2.set_title('初期値$x_0 = {}$（ランダム）のときのロジスティック写像の分岐図'.format(round(self.x, 3)))
         self.ax2.set_xlabel('$r$')
         self.ax2.set_ylabel('$x_n$')
         for i in range(len(self.r2)):
@@ -69,8 +74,7 @@ class Ctest4():
                 self.ax2.scatter(result[1], result[0],  color='b', s=1)
 
     def save_fig(self):
-        file_path = 'Week4/images/'
-        plt.savefig(file_path + 'ctest4', dpi=300)
+        plt.savefig(self.filepath + 'ctest4', dpi=300)
         plt.show()
 
     def do_plot(self):
@@ -78,6 +82,65 @@ class Ctest4():
         self.code_problem2()
         self.save_fig()
 
+class Report_3():
+    def __init__(self, r: float, s: str) -> None:
+        self.r = r
+        self.s = s
+        self.x = random.uniform(0, 1)
+        self.xn = np.linspace(0, 1, 1000)
+        self.filepath = "複雑系科学演習/Week4/images/"
+
+    def logistic(self, x: float, cnt: int) -> list:
+        num = x
+        num_array = [x]
+        for i in range(cnt):
+            num = self.r * num * (1 - num)
+            num_array.append(num)
+        return num_array
+
+    def pre_processing(self, cnt: int) -> float:
+        '''初期変動の影響をなくすための前処理'''
+        num = self.x
+        for i in range(cnt):
+            num = self.r * num * (1 - num)
+        return num
+
+    def plot_return_map(self):
+        '''初期変動が影響しないリターンマップの描画'''
+        plt.figure(figsize=(6, 6))
+        # 処理
+        x = self.pre_processing(150)
+        n = self.logistic(x, 50)
+        xn_array = []
+        for i in self.xn:
+            xn_array.append(self.r * (1 - i) * i)
+        # クモの巣図用の配列
+        spiper_plot_x = []
+        spiper_plot_y = []
+        for i in range(1, len(n)):
+            spiper_plot_x.append(n[i - 1])
+            spiper_plot_x.append(n[i])
+            spiper_plot_y.append(n[i])
+            spiper_plot_y.append(n[i])
+        
+        plt.plot(spiper_plot_x, spiper_plot_y, marker='o', linestyle='dashed')
+        plt.plot(self.xn, self.xn)
+        plt.plot(self.xn, xn_array)
+        plt.title("$r = $" + str(self.r) + ", $x_0 = $" + str(round(self.x, 3)) + '（ランダム）における\n' +\
+            '$x_n (150 < n < 200)$のときのリターンマップ')
+        plt.xlim(0, 1)
+        plt.ylim(0, 1)
+        plt.xlabel("$x_n$")
+        plt.ylabel("$x_{n+1}$")
+        plt.savefig(self.filepath + self.s, dpi=300)
+
+
+r = [1.50, 2.60, 3.20, 3.50, 3.86, 3.90]
 
 demo = Ctest4()
 demo.do_plot()
+'''
+for i in range(len(r)):
+    demo = Report_3(r[i], "report4_{}".format(i + 1))
+    demo.plot_return_map()
+'''
