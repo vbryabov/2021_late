94b7fecd8eaa95349568dd1bc8df75ae787160e8
Nishin-0141 <shinnosukepotato@gmail.com>
2021-11-26 15:37:35 +0900

    Week8 終了

--- 複雑系科学演習/Week8/task8_1.py
+++ 複雑系科学演習/Week8/task8_1.py
@@ -1,10 +1,13 @@
 from matplotlib import pyplot as plt
+from random import uniform
+
 
 def henon(x: float, y: float):
     return y + 1 - a * x ** 2, b * x
 
-x = 0.1
-y = 0
+
+x = uniform(-1, 1)
+y = uniform(-1, 1)
 a = 1.4
 b = 0.3
 cnt = 30000
@@ -16,9 +19,10 @@ for i in range(cnt):
         continue
     x_array.append(x)
     y_array.append(y)
-plt.figure(figsize=(8, 8))
+plt.figure(figsize=(10, 8))
 plt.plot(x_array, y_array, linestyle='None', marker='.', markersize=1)
 plt.title('Attractor of "Henon map"')
 plt.xlim(-2, 2)
 plt.ylim(-0.5, 0.5)
-plt.show()
+# plt.show()
+plt.savefig('複雑系科学演習/Week8/images/task8_1', dpi=300)
--- 複雑系科学演習/Week8/task8_2.py
+++ 複雑系科学演習/Week8/task8_2.py
@@ -1,24 +1,36 @@
 from matplotlib import pyplot as plt
 import numpy as np
+from random import uniform
 
 
 def henon(a: float, x: float, y: float):
+    """エノン写像の(x, y)座標を返す関数"""
     return y + 1 - a * x ** 2, b * x
 
 
-a = np.linspace(0, 1.5, 150, dtype=object)
-b = 0.3
-x = 0.1
-y = 0
-x_array = []
-y_array = []
-for i in a:
+a = np.arange(0, 1.5, 0.01, dtype=object)   # 課題２の a の範囲
+b = 0.3                                     # b = 0.3 に固定
+x0 = uniform(-1, 1)                         # ランダムの初期値x0（[-1, 1]の範囲）
+y0 = uniform(-1, 1)                         # ランダムの初期値y0（[-1, 1]の範囲）
+x_array = []                                # x の値を保存するための配列
+a_array = []                                # a の値を保存するための配列
+
+for i in a:                                 # 各 a のときの振る舞いを計算する
+    x = x0
+    y = y0
     for j in range(500):
         x, y = henon(i, x, y)
-        if j < 249: continue
+        if x < -1.5 or 1.5 < x:             # プロットの範囲外なら終了（オーバーフローを避けるため）
+            break
+        if j < 249:                         # 250 < xn < 500 の範囲外なら配列に入れない
+            continue
         x_array.append(x)
-        y_array.append(y)
+        a_array.append(i)
 
-plt.figure(figsize=(8, 8))
-plt.plot(x_array, y_array, linestyle='None', marker='.', markersize=10)
-plt.show()
+plt.figure(figsize=(10, 8))
+plt.plot(a_array, x_array, linestyle='None', marker='.', markersize=1)
+plt.title('Branching diagram of "Henon map"')
+plt.xlim(0, 1.6)
+plt.ylim(-1.5, 1.5)
+# plt.show()
+plt.savefig('複雑系科学演習/Week8/images/task8_2', dpi=300)
