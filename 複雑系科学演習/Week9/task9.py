0d02e78f6381787b2ac308cec668d567a8e68c69
Nishin-0141 <shinnosukepotato@gmail.com>
2021-11-30 17:31:02 +0900

    修正

--- 複雑系科学演習/Week9/task9.py
+++ 複雑系科学演習/Week9/task9.py
@@ -1,46 +1,22 @@
 from matplotlib import pyplot as plt
 import numpy as np
-from random import uniform
 
+filepath = '複雑系科学演習/Week9/ctest9.txt'
+x_array = []
+y_array = []
+with open(filepath, encoding='UTF-8') as f:
+    while line := f.readline():
+        line = line.rstrip()
+        x, y = line.split(' ')
+        x_array.append(int(x))
+        y_array.append(int(y))
 
-def henon(x: float, y: float):
-    return y + 1 - a * x ** 2, b * x
-
-
-def check_in_grid(cnt: int) -> int:
-    """分割したマスに点が入っている個数を返す"""
-    x = 0.1
-    y = 0
-
-    grid_x = np.linspace(-1.5, 1.5, cnt+1, dtype=object)
-    grid_y = np.linspace(-0.5, 0.5, cnt+1, dtype=object)
-    check = [[False]*cnt for _ in range(cnt)]
-    grid_cnt = 0
-    """
-    for i in range(len(grid_x)-1):
-        for j in range(len(grid_y)-1):
-            if (grid_x[i] <= x <= grid_x[i + 1]
-                    and grid_y[j] <= y <= grid_y[j + 1]):
-                check[i][j] = True
-    """
-    for i in range(3000):
-        x, y = henon(x, y)
-        if i < 249:
-            continue
-        for j in range(cnt):
-            for k in range(cnt):
-                if (grid_x[j] <= x <= grid_x[j + 1]
-                        and grid_y[k] <= y <= grid_y[k + 1]):
-                    check[j][k] = True
-    for i in range(cnt):
-        for j in range(cnt):
-            if check[i][j]:
-                grid_cnt += 1
-    return grid_cnt
-
-
-a = 1.4
-b = 0.3
-cnt = [10, 50, 100, 500]
-for i in cnt:
-    print(check_in_grid(i))
+plt.figure(figsize=(8, 8))
+plt.xlim(0, 500)
+plt.ylim(0, 5000)
+plt.plot(x_array, y_array)
+plt.title('Double-log graph')
+plt.xlabel('$N$')
+plt.ylabel('$m(N)$')
+# plt.show()
+plt.savefig('複雑系科学演習/Week9/images/task9', dpi=300)
